3dfd1f4ae9abcba2be62d1d8dc072d56e97c53cd
Jcaob <jcaobcatalan943@gmail.com>
2023-09-06 15:59:31 -0400

    weather app update

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const baseURL = `https://api.weatherapi.com/v1/current.json?key=`
+const baseURL = `http://api.weatherapi.com/v1/forecast.json?key=`
 const apikey = `1853ae2cd91045efb24204748232808`
 //https://api.weatherapi.com/v1/forecast.json?key=1853ae2cd91045efb24204748232808&q=46112&days=4&aqi=no&alerts=no
 /**
@@ -30,17 +30,28 @@ function initListeners() {
         let zip = $("#zip").val();
 
         if(city != ''){
-            let cityURL = baseURL + apikey + "&q=" + city + "&days=4&aqi=no&alerts=no"
+            let cityURL = baseURL + apikey + "&q=" + city + "&days=5&aqi=no&alerts=no"
             $.getJSON(cityURL, (data) =>{
                 console.log(data)
+                let current = data.current
+                let forecast = data.forecast.forecastday
+                $(".current").append(`<h1>${current.temp_f}F or ${current.temp_c}C</h1>`);
+                $.each(forecast, (idx, forecastday) =>{
+                    console.log(`index ${idx} forcast ${forecastday.day.avgtemp_f}`)
+                    $(".forecast").append(`<p>${forecastday.day.avgtemp_f}</p>`)
+                })
             }).fail(function(e) {
                 console.log( "you have failed at getting data", e);
               })
         }
 
         if(zip != ''){
-            let zipURL = baseURL + apikey + "&q=" + zip + "&days=4&aqi=no&alerts=no"
-            console.log(zipURL)
+            let zipURL = baseURL + apikey + "&q=" + zip + "&days=5&aqi=no&alerts=no"
+            $.getJSON(zipURL, (data) =>{
+                console.log(data)
+            }).fail(function(e) {
+                console.log( "you have failed at getting data", e);
+              })
         }
 
         
